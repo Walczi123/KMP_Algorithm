dbbb2e4819df56e845689c7a09d22c2ca47d0624
Patryk <patryk.wal@onet.pl>
2021-03-18 12:06:52 +0100

    v0.1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace AZ_KMP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //string text = "Ala ma kota, a kot ma Ale";
+            //string pattern = "kota";
+            string text = "abcdefg rightleft efgleftpowko";
+            string pattern = "efgleft";
+            var result = Algorithms.KMP(text, pattern);    
+            if(result!= -1)
+            {
+                Console.WriteLine($"Found pattern at index {result}.");
+            }
+            else
+            {
+                Console.WriteLine("Pattern not found.");
+            }
             Console.ReadKey();
         }
     }
